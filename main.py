4f16d6edf78ce8c7d659431d6786052ab8f4c8d1
JerrenM <jerrenminck@yahoo.com>
2025-03-08 13:03:23 -0600

    project finished!

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from stats import get_num_words, count_chars, sort_dict
-# import os
-# print("Current working directory:", os.getcwd())
+import sys
+
 def get_books_text(filepath):
 	with open(filepath) as f:
 		file_contents = f.read()
 	return file_contents
 
 def main():
-	filepath = "bookbot/books/frankenstein.txt"
+	filepath = sys.argv[1] 
 	text = get_books_text(filepath)
 	word_count = get_num_words(text)
 	dict = count_chars(text)
@@ -23,7 +23,11 @@ def main():
 		char = item["key"]
 		count = item["value"]
 		print(f"{char}: {count}")
-	
+
 	print("============= END ===============")
 
-main()
+if len(sys.argv) == 1:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+else:
+	main()
